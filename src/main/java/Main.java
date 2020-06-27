3156aa1b1e8aa6b200962290ce878f9404bb66a3
palimer6 <himmelpalimer@web.de>
2020-06-27 22:50:54 +0200

    implemented !timezones

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -7,13 +9,18 @@ import sensitiveinfo.ApiKeys;
 
 import javax.security.auth.login.LoginException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT)
                 .setToken(ApiKeys.TOKEN)
+                .setGame(Game.watching("type !help for commands"))
                 .addEventListener(new Main());
         builder.buildAsync();
     }
@@ -26,25 +33,136 @@ public class Main extends ListenerAdapter {
         if (botCheck(event)) {
             return;
         }
-        String rawCommand = event.getMessage().getContentRaw();
+        parseCommand(event);
+    }
+
+    private void parseCommand(MessageReceivedEvent event, String rawCommand) {
         String commandContent = rawCommand.replaceFirst("!", "");
         List<String> commandParts = Arrays.stream(commandContent.split("\\s"))
-                .filter(s -> !s.equals(""))
+                .filter(s -> !s.isEmpty())
                 .collect(Collectors.toList());
         String commandSubContent = commandContent.replaceFirst(commandParts.get(0) + "\\s", "");
-        System.out.printf("command from %#s: %s%n",
-                event.getAuthor(),
-                rawCommand);
-        if (commandParts.get(0).equals("ping")) {
-            sendMessage(event, "Pong!");
+        String commandRoot = commandParts.get(0);
+        List<String> commandModifiers;
+        if (commandParts.size() > 1) {
+            commandModifiers = commandParts.subList(1, commandParts.size());
+        } else {
+            commandModifiers = Collections.emptyList();
         }
-        if (commandParts.get(0).equals("say")) {
-            sendMessage(event, commandSubContent);
+        System.out.printf("command from %#s in %s channel %#s: %s%n",
+                event.getAuthor(),
+                event.getChannelType(),
+                event.getChannel(),
+                rawCommand);
+        boolean commandRecognized = true;
+        switch (commandRoot) {
+            case "ping" -> commandRecognized = ping(event);
+            case "say" -> commandRecognized = say(event, commandSubContent);
+            case "timezones" -> commandRecognized = timezones(event, commandModifiers);
+            case "convert", "help", "botinfo" -> System.out.printf("!%s not yet implemented%n", commandRoot);
+            default -> System.out.printf("invalid comment: !%s", commandRoot);
+        }
+        if (!commandRecognized) {
+            parseCommand(event, String.format("!help %s", commandRoot));
         }
     }
 
+    private void parseCommand(MessageReceivedEvent event) {
+        parseCommand(event, event.getMessage().getContentRaw());
+    }
+
+    private boolean ping(MessageReceivedEvent event) {
+        sendMessage(event, "Pong!");
+        return true;
+    }
+
+    private boolean say(@NotNull MessageReceivedEvent event, String text) {
+        if (event.getChannelType() == ChannelType.TEXT) {
+            event.getMessage().delete().queue();
+        }
+        sendMessage(event, text);
+        return true;
+    }
+
+    private boolean convert(MessageReceivedEvent event, List<String> commandModifiers) {
+        return false;
+    }
+
+    private boolean timezones(MessageReceivedEvent event, List<String> commandModifiers) {
+        int modifierCount = commandModifiers.size();
+        if (modifierCount < 2) {
+            return false;
+        }
+        String fromZone = commandModifiers.get(0);
+        int time;
+        Matcher matcher = Pattern.compile("(-?\\d+)([PpAa][Mm]?)?").matcher(commandModifiers.get(1));
+        if (matcher.find()) {
+            time = Integer.parseInt(matcher.group(1));
+            if (matcher.group(2) != null) {
+                time = switch (matcher.group(2)) {
+                    case "AM" -> mod(time, 12);
+                    case "PM" -> mod(time, 12) + 12;
+                    default -> time;
+                };
+            }
+        } else {
+            return false;
+        }
+        int fromUtcOffset = Integer.MAX_VALUE;
+        for (Map.Entry<String, Integer> zone : Zones.ZONES_MAP) {
+            if (fromZone.matches(zone.getKey())) {
+                fromUtcOffset = zone.getValue();
+            }
+        }
+        if (fromUtcOffset == Integer.MAX_VALUE) {
+            return false;
+        }
+        if (modifierCount > 2) {
+            String toZone = commandModifiers.get(2);
+            int toUtcOffset = Integer.MAX_VALUE;
+            for (Map.Entry<String, Integer> zone : Zones.ZONES_MAP) {
+                if (toZone.matches(zone.getKey())) {
+                    toUtcOffset = zone.getValue();
+                }
+            }
+            if (toUtcOffset == Integer.MAX_VALUE) {
+                return false;
+            }
+            int finalTime = calculateTime(time, fromUtcOffset, toUtcOffset);
+            sendMessage(event,
+                    String.format("%s: %d\n%s: %d",
+                            fromZone, mod(time, 24),
+                            toZone, mod(finalTime, 24)));
+            return true;
+        } else {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Map.Entry<String, Integer> zone : Zones.ZONES_MAP) {
+                if (!zone.getKey().matches(".*\\d.*")) {
+                    int finalTime = calculateTime(time, fromUtcOffset, zone.getValue());
+                    stringBuilder.append(String.format("%s: %d\n",
+                            zone.getKey(), mod(finalTime, 24)));
+                }
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            sendMessage(event, stringBuilder.toString());
+            return true;
+        }
+    }
+
+    private int calculateTime(int time, int fromUtcOffset, int toUtcOffset) {
+        return time - fromUtcOffset + toUtcOffset;
+    }
+
+    private boolean botinfo(MessageReceivedEvent event) {
+        return false;
+    }
+
+    private boolean help(MessageReceivedEvent event, List<String> commandModifiers) {
+        return false;
+    }
+
     private boolean isCommand(@NotNull MessageReceivedEvent event) {
-        return event.getMessage().getContentRaw().matches("!.*");
+        return event.getMessage().getContentRaw().matches("!\\S.*");
     }
 
     private void sendMessage(@NotNull MessageReceivedEvent event, String message) {
@@ -63,5 +181,13 @@ public class Main extends ListenerAdapter {
                     event.getMessage().getContentRaw());
         }
         return isBot;
+    }
+
+    private int mod(int a, int b) {
+        int ret = a % b;
+        if (ret < 0) {
+            ret += b;
+        }
+        return ret;
     }
 }
---
+++ src/main/java/Zones.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Zones {
+    public static final SortedSet<Map.Entry<String, Integer>> ZONES_MAP =
+            new TreeSet<>((o1, o2) -> {
+                int compareVal = Integer.compare(o1.getValue(), o2.getValue());
+                if (compareVal == 0) {
+                    return o1.getKey().compareTo(o2.getKey());
+                } else {
+                    return compareVal;
+                }
+            });
+
+    static {
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*12", -12));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*11", -11));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*10", -10));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*9", -9));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*8", -8));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("PST", -8));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*7", -7));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("MST", -7));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*6", -6));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("CST", -6));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*5", -5));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("EST", -5));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*4", -4));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*3", -3));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*2", -2));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("-0*1", -1));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("GMT", 0));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("[\\+-±]0+", 0));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("CET", 1));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*1", 1));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*2", 2));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*3", 3));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*4", 4));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*5", 5));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*6", 6));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*7", 7));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*8", 8));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*9", 9));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*10", 10));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*11", 11));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*12", 12));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*13", 13));
+        ZONES_MAP.add(new AbstractMap.SimpleEntry<>("\\+0*14", 14));
+
+    }
+
+}
