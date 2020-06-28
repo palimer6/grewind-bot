36c92052912c83d85f722485f109440812f38b69
palimer6 <himmelpalimer@web.de>
2020-06-28 11:20:31 +0200

    implemented !botinfo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,43 @@
 import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.requests.restaction.MessageAction;
 import org.jetbrains.annotations.NotNull;
 import sensitiveinfo.ApiKeys;
 
 import javax.security.auth.login.LoginException;
+import java.awt.Color;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main extends ListenerAdapter {
-    public static void main(String[] args) throws LoginException {
+    private static User grewindBot;
+    private static long upDate;
+
+    public static void main(String[] args) {
         JDABuilder builder = new JDABuilder(AccountType.BOT)
                 .setToken(ApiKeys.TOKEN)
                 .setGame(Game.watching("I can't !help you yet. Pali is too lazy because STUPID INTELLIJ DOESN'T WANT TO HAVE BLOCK STRINGS WORK YET"))
                 .addEventListener(new Main());
-        builder.buildAsync();
+        try {
+            grewindBot = builder.buildBlocking().getSelfUser();
+            upDate = System.currentTimeMillis();
+        } catch (InterruptedException | LoginException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -36,7 +51,7 @@ public class Main extends ListenerAdapter {
         parseCommand(event);
     }
 
-    private void parseCommand(MessageReceivedEvent event, String rawCommand) {
+    private void parseCommand(MessageReceivedEvent event, @NotNull String rawCommand) {
         String commandContent = rawCommand.replaceFirst("!", "");
         List<String> commandParts = Arrays.stream(commandContent.split("\\s"))
                 .filter(s -> !s.isEmpty())
@@ -59,7 +74,9 @@ public class Main extends ListenerAdapter {
             case "ping" -> commandRecognized = ping(event);
             case "say" -> commandRecognized = say(event, commandSubContent);
             case "timezones" -> commandRecognized = timezones(event, commandModifiers);
-            case "convert", "help", "botinfo" -> System.out.printf("!%s not yet implemented%n", commandRoot);
+            case "botinfo" -> commandRecognized = botInfo(event);
+            case "convert" -> commandRecognized = convert(event, commandModifiers);
+            case "help" -> commandRecognized = help(event, commandModifiers);
             default -> System.out.printf("invalid comment: !%s", commandRoot);
         }
         if (!commandRecognized) {
@@ -72,7 +89,7 @@ public class Main extends ListenerAdapter {
     }
 
     private boolean ping(MessageReceivedEvent event) {
-        sendMessage(event, "Pong!");
+        sendMessage(event, "Pong!", s -> event.getChannel().sendMessage(s));
         return true;
     }
 
@@ -80,7 +97,7 @@ public class Main extends ListenerAdapter {
         if (event.getChannelType() == ChannelType.TEXT) {
             event.getMessage().delete().queue();
         }
-        sendMessage(event, text);
+        sendMessage(event, text, s -> event.getChannel().sendMessage(s));
         return true;
     }
 
@@ -88,7 +105,7 @@ public class Main extends ListenerAdapter {
         return false;
     }
 
-    private boolean timezones(MessageReceivedEvent event, List<String> commandModifiers) {
+    private boolean timezones(MessageReceivedEvent event, @NotNull List<String> commandModifiers) {
         int modifierCount = commandModifiers.size();
         if (modifierCount < 2) {
             return false;
@@ -132,8 +149,8 @@ public class Main extends ListenerAdapter {
             sendMessage(event,
                     String.format("%s: %d\n%s: %d",
                             fromZone, mod(time, 24),
-                            toZone, mod(finalTime, 24)));
-            return true;
+                            toZone, mod(finalTime, 24)),
+                    s -> event.getChannel().sendMessage(s));
         } else {
             StringBuilder stringBuilder = new StringBuilder();
             for (Map.Entry<String, Integer> zone : Zones.ZONES_MAP) {
@@ -144,17 +161,37 @@ public class Main extends ListenerAdapter {
                 }
             }
             stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-            sendMessage(event, stringBuilder.toString());
-            return true;
+            sendMessage(event, stringBuilder.toString(), s -> event.getChannel().sendMessage(s));
         }
+        return true;
     }
 
     private int calculateTime(int time, int fromUtcOffset, int toUtcOffset) {
         return time - fromUtcOffset + toUtcOffset;
     }
 
-    private boolean botinfo(MessageReceivedEvent event) {
-        return false;
+    private boolean botInfo(MessageReceivedEvent event) {
+        long upTime = System.currentTimeMillis() - upDate;
+        LocalDateTime creationTime = grewindBot.getCreationTime().toLocalDateTime();
+        EmbedBuilder embedBuilder = new EmbedBuilder()
+                .setDescription("Bot Information")
+                .setColor(new Color(0xeb8c3f))
+                .setThumbnail(grewindBot.getAvatarUrl())
+                .addField("Bot Name", grewindBot.getName(), false)
+                .addField("Created on",
+                        String.format("%04d-%02d-%02d %02d:%02d",
+                                creationTime.getYear(),
+                                creationTime.getMonthValue(),
+                                creationTime.getDayOfMonth(),
+                                creationTime.getHour(),
+                                creationTime.getMinute()), false)
+                .addField("Current Up-Time",
+                        String.format("%d:%02d:%02d",
+                                TimeUnit.MILLISECONDS.toHours(upTime),
+                                TimeUnit.MILLISECONDS.toMinutes(upTime) % 60,
+                                TimeUnit.MILLISECONDS.toSeconds(upTime) & 60), false);
+        sendMessage(event, embedBuilder.build(), e -> event.getChannel().sendMessage(e));
+        return true;
     }
 
     private boolean help(MessageReceivedEvent event, List<String> commandModifiers) {
@@ -165,16 +202,16 @@ public class Main extends ListenerAdapter {
         return event.getMessage().getContentRaw().matches("!\\S.*");
     }
 
-    private void sendMessage(@NotNull MessageReceivedEvent event, String message) {
+    private <T> void sendMessage(@NotNull MessageReceivedEvent event, @NotNull T message,
+                                 @NotNull Function<T, MessageAction> method) {
         System.out.printf("sending message in channel %s: %s%n",
-                event.getChannel().getName(), message);
-        event.getChannel().sendMessage(message).queue();
+                event.getChannel().getName(), message.toString());
+        method.apply(message).queue();
     }
 
     private boolean botCheck(@NotNull MessageReceivedEvent event) {
         boolean isBot = event.getAuthor().isBot();
-        if ((!event.getAuthor().getName().equals("Grewind Bot") ||
-                !event.getAuthor().getDiscriminator().equals("6790"))
+        if (!event.getAuthor().equals(grewindBot)
                 && isBot) {
             System.err.printf("command from bot %#s: %s%n",
                     event.getAuthor(),
